3862478ec66d3f3653d3daf1355573e4ed2bdf46
lupandemo <lucasjackcosta18@gmail.com>
2020-07-10 17:44:13 -0300

    Inicando um Blog com React JS

---
+++ src/firebase.js
@@ -0,0 +1,46 @@
+import app from 'firebase/app';
+import 'firebase/database';
+import 'firebase/auth';
+
+
+    let firebaseConfig = {
+        apiKey: "AIzaSyCqotB8PgYrvPe9DF4sN-K14iaUBLoECBc",
+        authDomain: "reactapp-e1f5c.firebaseapp.com",
+        databaseURL: "https://reactapp-e1f5c.firebaseio.com",
+        projectId: "reactapp-e1f5c",
+        storageBucket: "reactapp-e1f5c.appspot.com",
+        messagingSenderId: "29143494506",
+        appId: "1:29143494506:web:b3a7021863b83372c73f13",
+        measurementId: "G-HXD6EFB2RJ"
+    };
+
+
+class Firebase{
+    constructor(){
+        app.initializeApp(firebaseConfig);
+    }
+
+    login(email, password){
+        return app.auth().signInWithEmailAndPassword(email, password)
+    }
+
+    async register(nome, email,password){
+        await app.auth().createUserWithEmailAndPassword(email, password)
+
+        const uid = app.auth().currentUser.uid;
+
+        return app.database().ref('usuarios').child(uid).set({
+            nome: nome
+        })
+    }
+
+    isInitialized(){
+        return new Promise(resolve => {
+            app.auth().onAuthStateChanged(resolve);
+        })
+    }
+
+
+}
+
+export default new Firebase();
